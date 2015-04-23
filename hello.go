94d1670a038174ffe56f3835c967d0353a22274f
Lucca <Lucca@ESPX-2.local>
2015-04-23 18:14:43 -0300

    Updated gitignore so we can add some new ansible files! (:
    We now have a VERY primitive playbook for the golang. Of course it will
    be better! For now, only runs on vagrant.
    
    Also need an deploy key, so if you want to clone it, fork it

--- hello.go
+++ hello.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Hi, Ansible! Here is your route: %q", html.EscapeString(r.URL.Path))
 	})
 
 	log.Fatal(http.ListenAndServe(":80", nil))
