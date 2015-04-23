cbdf40aa7a25a4bcde2cb91425d7f5dd3aa240aa
Lucca <Lucca@ESPX-2.local>
2015-04-23 20:23:17 -0300

    better and more easy to replicate tests, also just discovered that travis.yml is an hidden file... duh

--- hello.go
+++ hello.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-  "fmt"
-  "html"
-  "log"
-  "net/http"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
 )
 
 func mainHandler() http.HandlerFunc {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi Ansible and Travis! Route: %q", html.EscapeString(r.URL.Path))
-  })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi Ansible and Travis! Route: %q", html.EscapeString(r.URL.Path))
+	})
 }
 
 func main() {
-  http.HandleFunc("/", mainHandler())
-  log.Fatal(http.ListenAndServe(":80", nil))
+	http.HandleFunc("/", mainHandler())
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,19 +3,34 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 )
 
-type MockDd struct{}
+type HandleTester func(method string, params url.Values) *httptest.ResponseRecorder
+
+func GenerateHandleTester(t *testing.T, handleFunc http.Handler) HandleTester {
+	return func(method string, params url.Values) *httptest.ResponseRecorder {
+		req, err := http.NewRequest(method, "", strings.NewReader(params.Encode()))
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+		req.Header.Set(
+			"Content-Type",
+			"application/x-www-form-urlencoded; param=value",
+		)
+		w := httptest.NewRecorder()
+		handleFunc.ServeHTTP(w, req)
+		return w
+	}
+}
 
 func TestHome(t *testing.T) {
-	main := mainHandler()
-	req, _ := http.NewRequest("GET", "", nil)
-
-	w := httptest.NewRecorder()
-	main.ServeHTTP(w, req)
-
+	homeHandle := mainHandler()
+	test := GenerateHandleTester(t, homeHandle)
+	w := test("GET", url.Values{})
 	if w.Code != http.StatusOK {
-		t.Errorf("Home page not avaliable, status: %v", w.Code)
+		t.Errorf("Home page didn't return %v", http.StatusOK)
 	}
 }
